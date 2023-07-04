e4a4f7451023449f93375b32b04404c6f819c9fb
Oxana <110328307+CreativeOxana@users.noreply.github.com>
2023-07-04 10:06:24 +0200

    Add files via upload

--- javascript/index.js
+++ javascript/index.js
@@ -1,14 +1,14 @@
 function updateTime() {
-  let londonElement = document.querySelector("#london");
+  let londonElement = document.querySelector(".london");
   let londonDateElement = londonElement.querySelector(".date");
   let londonTimeElement = londonElement.querySelector(".time");
   let londonTime = moment().tz("Europe/London");
-  londonDateElement.textContent = londonTime.format("MMMM Do YYYY");
-  londonTimeElement.textContent = londonTime.format(
+  londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY");
+  londonTimeElement.innerHTML = londonTime.format(
     "h:mm:ss [<small>]A[</small>]"
   );
 
-  let romeElement = document.querySelector("#rome");
+  let romeElement = document.querySelector(".rome");
   let romeDateElement = romeElement.querySelector(".date");
   let romeTimeElement = romeElement.querySelector(".time");
   let romeTime = moment().tz("Europe/Rome");
